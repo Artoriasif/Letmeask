dcdf4c205f82f118bdfe751cf30a4ed4926ed088
Pedro Mendes <pedrohh0012@icloud.com>
2022-03-14 21:39:01 -0300

    Commit inicial

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button} from './components/Button';
+function App(){
+  return(
+    <>
+      <h1>Hello, world!</h1>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+    </>
+      
+  );
+}
+
+export default App;
